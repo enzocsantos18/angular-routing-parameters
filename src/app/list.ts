e027ca2d157347c1ae184eaec7898456f27f95f4
enzocsantos18 <enzocsantos18@gmail.com>
2022-03-11 20:15:33 -0300

    4 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -2,7 +2,8 @@ export const listData = [
   {
     codigo: 'ISD002A',
     nome: 'Arquitetura Orientada a Serviços',
-    diaSemana: '',
+    diaSemana: 'Quarta-feira',
+    horario: '19:00 às 22:30',
     ementa:
       'Construção de aplicações por meio da utilização de arquitetura orientada a serviços – SOA (Service-Oriented Architecture) e das principais tecnologias Web Services',
 
@@ -13,7 +14,8 @@ export const listData = [
   {
     codigo: 'ISW014A',
     nome: 'Projeto de Encontrabilidade',
-    diaSemana: '',
+    diaSemana: 'Segunda-feira',
+    horario: '19:00 às 22:30',
     ementa:
       'Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises',
 
@@ -24,15 +26,17 @@ export const listData = [
   {
     codigo: 'ITE004A',
     nome: 'Tópicos Especiais em Sistemas para Internet II',
-    diaSemana: '',
     ementa: 'Recursos e aplicações da tecnologia escolhida',
+    diaSemana: 'Sexta-feira',
+    horario: '19:00 às 22:30',
     objetivo:
       'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado',
   },
   {
     codigo: 'TTG401A',
     nome: 'Projeto de Trabalho de Graduação em Sistemas para Internet I',
-    diaSemana: '',
+    diaSemana: 'Sábado',
+    horario: '11:00 às 12:30',
     ementa:
       'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios',
     objetivo:
