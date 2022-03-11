1790f7bc9ac613dae7b5289ec3d6344fbf516630
enzocsantos18 <enzocsantos18@gmail.com>
2022-03-11 20:00:19 -0300

    3 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,35 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ListDetailsComponent } from './list-details/list-details.component';
+import { RouterModule } from '@angular/router';
+
+const ROUTES = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'aulas',
+    component: ListComponent,
+  },
+  {
+    path: 'aulas/:id',
+    component: ListDetailsComponent,
+  },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    ListComponent,
+    ListDetailsComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/list.ts
@@ -0,0 +1,41 @@
+export const listData = [
+  {
+    codigo: 'ISD002A',
+    nome: 'Arquitetura Orientada a Serviços',
+    diaSemana: '',
+    ementa:
+      'Construção de aplicações por meio da utilização de arquitetura orientada a serviços – SOA (Service-Oriented Architecture) e das principais tecnologias Web Services',
+
+    objetivo:
+      'Discutir tecnologias associadas a SOA (Services Oriented Architecture). Objetivos específicos. Estudar e utilizar a Arquitetura Orientada a Serviços e Web Services: padrões, protocolos e especificações, Frameworks e APIs (Application Programming Interface). A especificação BPEL (Business Proccess Execution Language) para composição de serviços. Web Services nas plataformas JavaEE (Java Enterprise Edition) e .Net e SOA RESTful  services. Desenvolver Web Services',
+  },
+
+  {
+    codigo: 'ISW014A',
+    nome: 'Projeto de Encontrabilidade',
+    diaSemana: '',
+    ementa:
+      'Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises',
+
+    objetivo:
+      'Explorar estratégias que direcionam o tráfego para um site, ajudam os usuários a encontrar conteúdo dentro de um site e encorajam consultas de retorno. Discutir os Métodos de Otimização de Search Engine (SEO), estratégias de marketing, análise de tráfego e teoria em torno comportamento de busca. Investigar as relações entre usabilidade, acessibilidade, arquitetura da informação, marketing, e encontrabilidade, a fim de implementar estratégias para a criação de conteúdo encontrável. Objetivos específicos.  Criar template para o Blog sobre encontrabilidade, apresentar resultados das pesquisas sobre os temas (técnicas e ou utilitários SEO, técnicas e ou utilitários de marketing por e-mail, Microformatos, busca, teoria da encontrabilidade, marketing online). Criar um search engine amigável. Criar conteúdo encontrável aplicando técnicas de acessibilidade e os padrões Web. Implantar um sítio Internet com todos os recursos estudados até o semestre',
+  },
+
+  {
+    codigo: 'ITE004A',
+    nome: 'Tópicos Especiais em Sistemas para Internet II',
+    diaSemana: '',
+    ementa: 'Recursos e aplicações da tecnologia escolhida',
+    objetivo:
+      'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado',
+  },
+  {
+    codigo: 'TTG401A',
+    nome: 'Projeto de Trabalho de Graduação em Sistemas para Internet I',
+    diaSemana: '',
+    ementa:
+      'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios',
+    objetivo:
+      'Apoiar o estudante na organização de seu portfólio de projetos desenvolvidos ao longo do curso. Objetivos específicos. Os estudantes devem ser capazes de organizar o portfólio individual dos projetos desenvolvidos ao longo do curso. Devem ser capazes de redigir resumos, artigos e relatórios técnicos dos trabalhos em linguagem apropriada e na forma culta. Deve demonstrar independência de pensamento e autonomia para pesquisa',
+  },
+];
